9e5ddcd1226a72dad7007516a81a52c2f01221a5
Jared Dunn <jdunn@webucator.com>
2024-05-30 17:58:46 -0400

    add corsheaders

--- numstowords/settings.py
+++ numstowords/settings.py
@@ -16,9 +16,8 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env(DEBUG=(bool, False))
-env_file = BASE_DIR / "numstowords/.env"
-environ.Env.read(str(env_file))
+env = environ.Env()
+env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -41,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    
+    "corsheaders.middleware.CorsMiddleware"
 ]
 
 ROOT_URLCONF = "numstowords.urls"
@@ -69,6 +72,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
 ]
 
 WSGI_APPLICATION = "numstowords.wsgi.application"
