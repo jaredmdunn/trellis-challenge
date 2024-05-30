49eb883ed0a7b0ea159f0cee2d26e54d511d95f2
Jared Dunn <jdunn@webucator.com>
2024-05-30 19:29:56 -0400

    fix zero errors

--- numstowords/utils.py
+++ numstowords/utils.py
@@ -65,10 +65,10 @@ def convert_num_to_english(num: int) -> str:
         return f"{nums_to_words_dict.get(num - num % 10, '')} {nums_to_words_dict.get(num % 10, '')}"
     
     if num < 1000:
-        return f"{nums_to_words_dict.get(num // 100,'')} hundred {convert_num_to_english(num % 100)}"
+        return f"{nums_to_words_dict.get(num // 100,'')} hundred {convert_num_to_english(num % 100) if num % 100 > 0 else ''}".strip()
 
     if num < mil:
-        return f"{convert_num_to_english(num // 1000)} thousand {convert_num_to_english(num % 1000)}"
+        return f"{convert_num_to_english(num // 1000)} thousand {convert_num_to_english(num % 1000) if num % 1000 > 0 else ''}".strip()
     
     if num < bil:
-        return f"{convert_num_to_english(num // mil)} million {convert_num_to_english(num % mil)}"
+        return f"{convert_num_to_english(num // mil)} million {convert_num_to_english(num % mil) if num % mil > 0 else ''}".strip()
